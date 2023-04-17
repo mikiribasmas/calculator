96f08467d1d3ab2101a4a2f1df20fdecd37826a5
mikiribasmas <miquel.ribas21@gmail.com>
2023-04-17 13:55:22 +0200

    adding functionalities

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,12 +1,32 @@
+const screen = document.getElementById('calculator-screen');
 const buttons = document.querySelectorAll('.func-button button');
-const screen = document.querySelector('.item-screen .screen');
-
+const equals = document.getElementById('equals-button');
+const reset = document.getElementById('ac')
 
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
-      const buttonText = button.textContent;
-      screen.innerHTML = buttonText;
-      console.log(`El botón ${buttonText} ha sido presionado`);
-    });
+  button.addEventListener('click', () => {
+    if (screen.value == 0) {
+      screen.value = button.dataset.value;
+    }
+    else if (button.dataset.value != '=' /*&& button.dataset.value === '0'*/) {
+      screen.value += button.dataset.value;
+    }
   });
-   
+});
+
+reset.addEventListener('click', () => {
+  screen.value = 0;}
+);
+
+equals.addEventListener('click', () => {
+
+  const expression = screen.value;
+
+  try {
+    screen.value = eval(expression);
+
+  } catch (error) {
+    screen.value = "Error: expresión no válida";
+  }
+});
+
